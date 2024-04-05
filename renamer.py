7be7289f81f98386db4053e0251b5a0f64b6c1f1
Jan Horák <7425857-shtrdl@users.noreply.gitlab.com>
2024-04-05 12:32:38 +0200

    Implement csv log output for seq lenght and name comparision

--- renamer.py
+++ renamer.py
@@ -1,4 +1,25 @@
-import argparse, os
+import argparse, os, csv
+from Bio import SeqIO
+
+
+def output_log_table(filename: str, old_names: list, output_filename: str) -> None:
+    with open(output_filename, "w", newline="") as csvfile:
+        fieldnames = ["seq_lengh_AA", "new_name", "old_name"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for i, record in enumerate(SeqIO.parse(filename, "fasta")):
+            seq_length_AA = len(record.seq.replace("-", ""))
+            new_name = record.name
+            old_name = old_names[i]
+            writer.writerow(
+                {
+                    "old_name": old_name,
+                    "new_name": new_name,
+                    "seq_lengh_AA": seq_length_AA,
+                }
+            )
+    print("Log table written to", output_filename)
 
 
 def translate_name(name: str, map: dict) -> str:
@@ -76,9 +97,19 @@ def write(output_folder: str, output_filename: str, content: list) -> None:
     return None
 
 
+def get_old_names(content: list) -> list:
+    old_names = []
+    for line in content:
+        if line.startswith(">"):
+            old_names.append(line.lstrip(">"))
+    return old_names
+
+
 def parse(input_folder: str, output_folder: str, overwrite: bool) -> None:
     for filename in os.listdir(input_folder):
         output_filename = filename.split(".")[0] + "_renamed.txt"
+        filename_path = os.path.join(input_folder, filename)
+        output_filename_path = os.path.join(output_folder, output_filename)
 
         if not overwrite and os.path.exists(
             os.path.join(output_folder, output_filename)
@@ -89,9 +120,19 @@ def parse(input_folder: str, output_folder: str, overwrite: bool) -> None:
         with open(os.path.join(input_folder, filename), "r") as f:
             content = f.read()
             content = content.splitlines()
+            old_names = get_old_names(content)
 
             parsed_lines = process_lines(content, get_protein_name(filename))
             write(output_folder, output_filename, parsed_lines)
+
+        log_table_name = os.path.join(
+            output_folder, filename.split(".")[0] + "_log.csv"
+        )
+        output_log_table(
+            output_filename_path,
+            old_names,
+            log_table_name,
+        )
 
     return None
 
